160d638f7a7b71ba8eaf835b7112cfae633d8dc6
Thomas <thomas@Thomass-MacBook-Pro-2.local>
2020-03-11 14:00:01 -0400

    video files

--- MultifoldApp/src/ofApp.cpp
+++ MultifoldApp/src/ofApp.cpp
@@ -1,18 +1,36 @@
 #include "ofApp.h"
 
+static int64_t prev_frame = 0;
+static int64_t cur_frame = 0;
+static uint8_t offset = 1;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     ofSetVerticalSync(false);
     ofSetFrameRate(120);
     ofBackground(0);
     
-    mVideoWapTest = inn::VideoWap::create();
+    mVideoWapTest = inn::VideoWarp::create();
     mVideoWapTest->loadVideo();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    if (cur_frame != prev_frame)
+    {
+        mVideoWapTest->update(cur_frame);
+        
+        prev_frame = cur_frame;
+    }
+    
+    cur_frame++;
+    
+    if (cur_frame >= mVideoWapTest->getTotalNumFrames())
+    {
+        cur_frame = 0;
+    }
+ 
+      HPV::Update();
 }
 
 //--------------------------------------------------------------
@@ -75,6 +93,7 @@ void ofApp::dragEvent(ofDragInfo dragInfo){
 
 }
 
+//--------------------------------------------------------------
 void ofApp::exit(){
         HPV::DestroyHPVEngine();
 }
